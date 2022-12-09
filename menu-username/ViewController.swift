64a415ac40a26b769f7a5666eca121c06376f29b
Manvendra Singh <manvendra.singh@raksul.com>
2022-12-09 19:43:51 +0530

    Initial Commit

---
+++ menu-username/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  menu-username
+//
+//  Created by Manvendra Singh on 09/12/2022.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
